53e7c01c2c277c0d9d1636829d899794996efde2
Izzy Pedrizco-Carranz <ip7431@rit.edu>
2020-11-15 23:35:04 -0500

    Add project files.

---
+++ ReadingFromFiles/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace ReadingFromFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = null;
+            string path = "in.txt";
+            //string path = "pf2.txt";
+            //using (StreamReader reader = new StreamReader("C:\\Users\\Israe\\Documents\\ForICE.txt"))
+            //Or
+            if (File.Exists(path))
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    while ((str = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(str);
+                    }
+                }
+                Console.WriteLine(File.GetCreationTime(path));
+                Console.WriteLine(File.GetLastWriteTime(path));
+                Console.WriteLine($"\n{File.ReadAllText(path)}\n");
+                string[] readText = File.ReadAllLines(path);
+                foreach (string s in readText)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            else
+            {
+                Console.WriteLine("File is not found");
+            }
+
+        }
+    }
+}
